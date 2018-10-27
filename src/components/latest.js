c1373163113a8c9e571adb854c7adef7fb103b22
frdvorak <frantisek.dvorak011@gmail.com>
2018-10-27 13:29:06 +0100

    async/await API call working

--- src/components/latest.js
+++ src/components/latest.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 class Latest extends Component {
+    componentDidMount = async () => {
+        // call api, convert it to JSON, save that in variable 'data'
+        const api_call = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=09b4242d1a2847b1b520eeb23adabd9d');
+        const data = await api_call.json();
+        console.log(data.articles[0]);
+        
+      };
     render(){
         return(
-            <div><h1>Latest Page</h1></div>
+            <div>
+                <h1>Latest Page</h1>
+                <a href='https://newsapi.org/'>https://newsapi.org/</a>
+
+                <p></p>
+            </div>
         )
     }
 }
