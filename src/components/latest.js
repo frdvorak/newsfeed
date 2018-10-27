f209d9450e9e6a69b02383175d4958db2a63e5a1
frdvorak <frantisek.dvorak011@gmail.com>
2018-10-27 11:31:00 +0100

    added 3 components

---
+++ src/components/latest.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Latest extends Component {
+    render(){
+        return(
+            <div><h1>Latest Page</h1></div>
+        )
+    }
+}
+
+export default Latest;
---
+++ src/components/other.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Other extends Component {
+    render(){
+        return(
+            <div><h1>Other Page</h1></div>
+        )
+    }
+}
+
+export default Resume;
