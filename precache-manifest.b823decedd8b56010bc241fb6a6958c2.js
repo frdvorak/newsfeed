ca0fc3fd95e537ba935c5183764b85be968a4e88
frdvorak <frantisek.dvorak011@gmail.com>
2018-11-04 16:17:51 +0000

    Updates

---
+++ precache-manifest.b823decedd8b56010bc241fb6a6958c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5591eaa0d527a768f984",
+    "url": "/newsfeed/static/js/runtime~main.5591eaa0.js"
+  },
+  {
+    "revision": "8d63bd635b776e070984",
+    "url": "/newsfeed/static/js/main.8d63bd63.chunk.js"
+  },
+  {
+    "revision": "fedd5c25976a4469edad",
+    "url": "/newsfeed/static/js/1.fedd5c25.chunk.js"
+  },
+  {
+    "revision": "8d63bd635b776e070984",
+    "url": "/newsfeed/static/css/main.78f505c8.chunk.css"
+  },
+  {
+    "revision": "fedd5c25976a4469edad",
+    "url": "/newsfeed/static/css/1.98681ef3.chunk.css"
+  },
+  {
+    "revision": "556820774fe1b7fa5c4a4a51246666f7",
+    "url": "/newsfeed/index.html"
+  }
+];
